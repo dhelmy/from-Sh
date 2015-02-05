4dd9b092913b2e4ff910cc1ced6cdf9b1b03f6de
Dia <dia@DiaMac.local>
2015-02-04 20:44:52 -0500

    Initial Commit

---
+++ from Sh/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  from Sh
+//
+//  Created by Dia on 2/4/15.
+//  Copyright (c) 2015 SH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
